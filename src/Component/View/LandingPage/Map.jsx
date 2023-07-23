7d33f41b0fa65ca3cef4d651ce2e8b74a2bac9f8
GentaArya <mgentaarya@yahoo.com>
2023-07-23 21:38:46 +0700

    update-maps

--- src/Component/View/LandingPage/Map.jsx
+++ src/Component/View/LandingPage/Map.jsx
@@ -28,24 +28,39 @@ const Map = () => {
   const handleMarkerClick = () => {
     const lat = centerCoordinates[0];
     const lng = centerCoordinates[1];
-    window.open(`https://www.google.com/maps/search/?api=1&query=${lat},${lng}`, "_blank");
+    window.open(
+      `https://www.google.com/maps/search/?api=1&query=${lat},${lng}`,
+      "_blank"
+    );
   };
 
   return (
     <div className="flex justify-center items-center pl-5 pr-5 pb-1">
       <div className="w-full h-96 rounded-md overflow-hidden shadow-md border border-gray-700">
-        <div className="text-center p-4 bg-gray-800 text-white font-bold">
-          Kantor NU Ketapang
+        <div className="text-center p-4 bg-green-800 text-white font-bold">
+          Maps
         </div>
-        <MapContainer center={centerCoordinates} zoom={17} className="w-full h-full">
+        <MapContainer
+          center={centerCoordinates}
+          zoom={17}
+          className="w-full h-full"
+        >
           <TileLayer
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           />
 
-          <Circle center={centerCoordinates} radius={55} className={isAnimating ? "circle-animation" : ""} />
+          <Circle
+            center={centerCoordinates}
+            radius={55}
+            className={isAnimating ? "circle-animation" : ""}
+          />
 
-          <Marker position={centerCoordinates} icon={customIcon} eventHandlers={{ click: handleMarkerClick }}>
+          <Marker
+            position={centerCoordinates}
+            icon={customIcon}
+            eventHandlers={{ click: handleMarkerClick }}
+          >
             <Popup>Nahdlatul Ulama Ketapang</Popup>
           </Marker>
         </MapContainer>
