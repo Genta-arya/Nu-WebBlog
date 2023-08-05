5454af0604a1746afed5e35120d0ef0f1a501145
GentaArya <mgentaarya@yahoo.com>
2023-08-06 05:06:44 +0700

    update-404

--- src/Component/View/Dashboard/Posting.jsx
+++ src/Component/View/Dashboard/Posting.jsx
@@ -169,7 +169,7 @@ const PostingArtikel = () => {
               onChange={handleThumbnailChange}
               accept="image/*"
               className="w-full border border-gray-300 px-4 py-2 rounded focus:outline-none focus:border-blue-500"
-              required
+              
             />
           </div>
 
